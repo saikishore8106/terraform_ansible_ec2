d4ba8b8a66991e08218c29ac234c72fc47efd2d2
saikishore8106 <145852787+saikishore8106@users.noreply.github.com>
2025-07-30 15:59:39 +0530

    Update generate_inventory.sh

--- terraform/generate_inventory.sh
+++ terraform/generate_inventory.sh
@@ -1,1 +1,16 @@
 
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+# extract IPs
+terraform output -json public_ips | jq -r '.[]' > ip_list.txt
+KEY_PATH=$(terraform output -raw private_key_path)
+
+echo "[ec2_instances]" > ../ansible/inventory.ini
+while read ip; do
+  echo "$ip ansible_user=ec2-user ansible_ssh_private_key_file=$KEY_PATH" >> ../ansible/inventory.ini
+done < ip_list.txt
+
+echo "Generated inventory.ini for Ansible:"
+cat ../ansible/inventory.ini
